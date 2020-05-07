33e31baae96ee9be2b0f1adb9d1992ec79cd70a5
Emily Ury <ury.emily@gmail.com>
2020-05-07 12:44:36 -0400

    added a file header

--- soils simple regression.R
+++ soils simple regression.R
@@ -1,7 +1,20 @@
 
+#' ---
+#' title: "Simple Regression of Soil Core Measurments"
+#' author: "Emily Ury"
+#' date: "May 7, 2020"
+#' output: github_document
+#' ---
+#'
 
-## regression for autocorrelation assessment
-## SNAP Carbon Story
+
+#' Created this script to look at general correlation 
+#' analyses between soil core data. May be used as a coarse
+#' check for *autocorrelated data*. **Not a final product.**
+
+
+## Plots all data from the soil core mastersheet
+
 
 ## file location
 setwd("C:/Users/eau6/Dropbox (Duke Bio_Ea)/My data/SNAP_compilation/SNAP_Carbon_Story")
