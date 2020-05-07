40b1d8a9a356f9035d02f12b04527f597d7e12bb
Emily Ury <ury.emily@gmail.com>
2020-05-07 16:04:48 -0400

    add info with factoextra

--- soils PCAs.R
+++ soils PCAs.R
@@ -104,7 +104,38 @@ title(main = "Site 5 (wet - cypress/mixed)", line = 1)
 
 
 
+#### some basic extra visualization using the package
+
+library(factoextra)
+fviz_eig(pca)
+fviz_pca_ind(pca,
+             col.ind = "cos2", # Color by the quality of representation
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE     # Avoid text overlapping
+)
+fviz_pca_var(pca,
+             col.var = "contrib", # Color by contributions to the PC
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE     # Avoid text overlapping
+)
+fviz_pca_biplot(pca, repel = TRUE,
+                col.var = "#2E9FDF", # Variables color
+                col.ind = "#696969"  # Individuals color
+)
 
 
+#### get the info out of the pca
+# Eigenvalues
+eig.val <- get_eigenvalue(res.pca)
+eig.val
 
-
+# Results for Variables
+res.var <- get_pca_var(res.pca)
+res.var$coord          # Coordinates
+res.var$contrib        # Contributions to the PCs
+res.var$cos2           # Quality of representation 
+# Results for individuals
+res.ind <- get_pca_ind(res.pca)
+res.ind$coord          # Coordinates
+res.ind$contrib        # Contributions to the PCs
+res.ind$cos2           # Quality of representation 
