bd31db67756759c9bdbee075b1a6aa38b26fb689
Emily Ury <ury.emily@gmail.com>
2020-05-07 13:35:51 -0400

    init

---
+++ soils PCAs.R
@@ -0,0 +1,14 @@
+#' ---
+#' title: "PCAs of all soil core measurements"
+#' author: "Emily Ury"
+#' date: "May 7, 2020"
+#' output: github_document
+#' ---
+#'
+
+
+#' Created this script to look at general the soil properties of
+#' SNAP soil cores,  how they are distributed across the three plots, 
+#' how the treatments are affecting these properties, etc...
+#' 
+#' Data is from the *soils mastersheet* from **2019**
