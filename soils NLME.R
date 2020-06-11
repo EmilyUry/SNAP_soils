97449e005fbe54d345028d89da0035ec0f36e052
Emily Ury <ury.emily@gmail.com>
2020-06-11 14:22:23 -0400

    init

---
+++ soils NLME.R
@@ -0,0 +1,13 @@
+#' ---
+#' title: "soils Linear Mixed Effects model with NLME"
+#' author: "Emily Ury"
+#' date: "May 12, 2020"
+#' output: github_document
+#' ---
+#'
+#' See the tutorial"
+#' https://crumplab.github.io/psyc7709/book/docs/a-tutorial-for-using-the-lme-function-from-the-nlme-package-.html
+#' 
+#' 
+
+library(nlme)
